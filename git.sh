3f42dd75c199d99721433ba5193cd97d22302ff8
krlex <krle@tilda.center>
2020-10-10 18:32:10 +0200

    remove some extra codes

--- git.sh
+++ git.sh
@@ -9,7 +9,7 @@ echo "Pulling in latest changes for all repositories..."
 # Find all git repositories and update it to the master latest revision
 for i in $(find . -name ".git" | cut -c 3-); do
     echo "";
-    echo "+$i+";
+    echo ">>>" $i "<<<";
 
     # We have to go to the .git parent directory to call the pull command
     cd "$i";
@@ -22,4 +22,4 @@ for i in $(find . -name ".git" | cut -c 3-); do
     cd $CUR_DIR
 done
 
-echo "Complete!"
+echo "$CUR_DIR Complete!"
